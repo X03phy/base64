304ac337efded751b3be530b6579cfd352f2691c
Elio Bonutto <elio.bonutto@gmail.com>
2025-07-17 12:04:15 +0200

    debut de la fct decode

--- include/base64.h
+++ include/base64.h
@@ -9,11 +9,6 @@
 
 static const char BASE64_TABLE[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-typedef struct s_base64_ctx
-{
-	uint8_t buffer[3];
-} t_base64_ctx;
-
-void base64_encode( const uint8_t *data, size_t len, uint8_t *ciphertext );
+void base64_encode( const uint8_t *text, size_t len, uint8_t *ciphertext );
 
 #endif
--- src/base64.c
+++ src/base64.c
@@ -1,37 +1,43 @@
 #include "base64.h"
 
-void base64_encode( const uint8_t *data, size_t len, uint8_t *ciphertext )
+void base64_encode( const uint8_t *text, size_t len, uint8_t *ciphertext )
 {
-	size_t data_index = 0;
+	size_t text_index = 0;
 	size_t ciphertext_index = 0;
 
-	while ( data_index < len )
+	while ( text_index < len )
 	{
-		ciphertext[ciphertext_index++] = BASE64_TABLE[( ( data[data_index] & 0xFC ) >> 2 )];
+		ciphertext[ciphertext_index++] = BASE64_TABLE[( ( text[text_index] & 0xFC ) >> 2 )];
 
-		if ( ( data_index + 1 ) == len )
+		if ( ( text_index + 1 ) == len )
 		{
-			ciphertext[ciphertext_index++] = BASE64_TABLE[( ( data[data_index] & 0x3 ) << 4 )];
+			ciphertext[ciphertext_index++] = BASE64_TABLE[( ( text[text_index] & 0x3 ) << 4 )];
 			ciphertext[ciphertext_index++] = '=';
 			ciphertext[ciphertext_index++] = '=';
 			break;
 		}
 
-		ciphertext[ciphertext_index++] = BASE64_TABLE[( ( ( data[data_index] & 0x3 ) << 4 ) |
-														( ( data[data_index + 1] & 0xF0 >> 4 ) ) )];
+		ciphertext[ciphertext_index++] = BASE64_TABLE[( ( ( text[text_index] & 0x3 ) << 4 ) |
+														( ( text[text_index + 1] & 0xF0 >> 4 ) ) )];
 
-		if ( ( data_index + 2 ) == len )
+		if ( ( text_index + 2 ) == len )
 		{
-			ciphertext[ciphertext_index++] = BASE64_TABLE[( ( data[data_index + 1] & 0xF ) << 2 )];
+			ciphertext[ciphertext_index++] = BASE64_TABLE[( ( text[text_index + 1] & 0xF ) << 2 )];
 			ciphertext[ciphertext_index++] = '=';
 			break;
 		}
 
-		ciphertext[ciphertext_index++] = BASE64_TABLE[( ( ( data[data_index + 1] & 0xF ) << 2 ) |
-														( ( data[data_index + 2] & 0xC0 ) >> 6 ) )];
-		ciphertext[ciphertext_index++] = BASE64_TABLE[( data[data_index + 2] & 0x3F )];
-		data_index += 3;
+		ciphertext[ciphertext_index++] = BASE64_TABLE[( ( ( text[text_index + 1] & 0xF ) << 2 ) |
+														( ( text[text_index + 2] & 0xC0 ) >> 6 ) )];
+		ciphertext[ciphertext_index++] = BASE64_TABLE[( text[text_index + 2] & 0x3F )];
+		text_index += 3;
 	}
 
 	ciphertext[ciphertext_index] = '\0';
 }
+
+void base64_encode( const uint8_t *ciphertext, size_t len, uint8_t *text )
+{
+
+}
+
